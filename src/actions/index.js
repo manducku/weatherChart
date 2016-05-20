2df3a13d876af695fd9c958031c6b6e80bf00b74
manducku <alwaysfun2183@gmail.com>
2016-05-20 16:21:54 +0900

    add fetch_weather (action creator with axios library)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY='v3d0aec4e3471a4a6a655501513ab847e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid={$API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetch_weather(city) {
+        const url = `{$ROOT_URL}&q=${city},us`;
+        const request = axios.get(url);
+
+        return {
+                type: FETCH_WEATHER,
+                payload: request 
+        };
+    }
