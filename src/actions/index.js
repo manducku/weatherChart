9e0bb8dde1efbd5cf2ae4bee9a2cf5cfca815f22
manducku <alwaysfun2183@gmail.com>
2016-05-20 16:59:05 +0900

    add connect API with weathermap.org

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-
-const API_KEY='v3d0aec4e3471a4a6a655501513ab847e';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid={$API_KEY}`;
+const API_KEY='3d0aec4e3471a4a6a655501513ab847e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast/?APPID=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetch_weather(city) {
-        const url = `{$ROOT_URL}&q=${city},us`;
+        console.log(city);
+        const url = `${ROOT_URL}&q=${city},us`;
         const request = axios.get(url);
-
+        console.log(url);
         return {
                 type: FETCH_WEATHER,
                 payload: request 
