ac3018de7709c721546f51272f75ccbbbfa29ed7
manducku <alwaysfun2183@gmail.com>
2016-05-20 15:17:09 +0900

    init seachbar state

--- src/container/search_bar.js
+++ src/container/search_bar.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {term: ''};
+        this.OnInputChange = this.OnInputChange.bind(this);
+    }
+
+    OnInputChange(event){
+        this.setState({term: event.target.value});
+    }
+
     render(){
         return(
             <form className="input-group">
-                <input />
+                <input 
+                    placeholder="도시를 입력하세요" 
+                    className="form-control" 
+                    value={this.state.term}
+                    onChange={this.OnInputChange}
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
