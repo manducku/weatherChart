1c8b1cb67909962c5f2e3ad3cf236dec7c31925d
manducku <alwaysfun2183@gmail.com>
2016-05-21 17:35:14 +0900

    add renderWeather

--- src/container/weather_list.js
+++ src/container/weather_list.js
@@ -1,17 +1,29 @@
 import React, {Component} from 'react';
-import {connect} from 'redux';
+import {connect} from 'react-redux';
 
 
 class WeatherList extends Component {
+    renderWeather(cityData){
+        return(
+            <tr>
+                <td>{cityData.city.name}</td>
+            </tr>
+
+        );
+    }
     render(){
         return(
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
+                        <th>Temparature</th>
+                        <th>Pressure</th>
+                        <th>Huminity</th>
                     </tr>
                 </thead>
                 <tbody>
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>     
             </table>
         );
